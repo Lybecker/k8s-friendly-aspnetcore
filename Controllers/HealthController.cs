943ef26f97d4a8ab59113dc6d18cebc366b47e34
Anders Lybecker <anlybeck@microsoft.com>
2019-06-21 13:36:59 +0200

    Create a health endpoint

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_docker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // If the process ready to serve requests e.g. done warmup of cache etc.
+        [HttpGet]
+        [Route("ready")]
+        public IActionResult Ready()
+        {
+            return Ok("Yes, ready now");
+        }
+
+        // Check that the ASP.NET runtime responds
+        [HttpGet]
+        public IActionResult Alive()
+        {
+            return Ok("Alive. All is well.");
+        }
+    }
+}
